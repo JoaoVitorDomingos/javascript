4368d6af7009ce29d703b96d9a440fc486fa6f80
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2023-07-11 11:29:26 -0300

    condições em js

---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = "Brail"
+
+console.log(`Vivendo em ${país}`)
+
+if (país == "Brasil") { // CONDIÇÃO COMPOSTA
+    console.log("Você é brasileiro")
+} else {
+    console.log("Você é estrangeiro!")
+}
