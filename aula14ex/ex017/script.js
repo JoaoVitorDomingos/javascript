309237e9b41260546333b5d5b0906000b1b36226
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2023-07-22 11:56:30 -0300

    ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert("Olá")
+function calcular() {
+    var numero = document.getElementById("txtn")
+    var tabuada = document.getElementById('seltab')
+
+    if (numero.value.length == 0) {
+        alert("Por favor digite um número")
+    } else {
+        var n = Number(numero.value)
+        var c = 1
+        tabuada.innerHTML = ""
+        while (c <= 10) {
+            var item = document.createElement("option")
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
+}
