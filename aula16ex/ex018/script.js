b3cc942ef54c5d766f0b89d8b73af5402cb559b1
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2023-07-30 15:59:38 -0300

    atualizado o ex018

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -26,44 +26,50 @@ function adicionar() {
         tabnumeros.appendChild(option)
         option.innerHTML = `Valor ${numero} adicionado.`
     }
+
+    txtnum.value = ""
+    txtnum.focus()
 }
 
 function finalizar() {
-    listanumeros.sort()
-    console.log(listanumeros)
 
-    var totalnumeros = listanumeros.length
-    console.log(`Ao todo temos ${totalnumeros} números cadastrados.`)
-    divres.innerHTML = `<p>Ao todo temos ${totalnumeros} números cadastrados.</p>`
+    if (listanumeros.length == 0) {
+        alert("Adicione os números!!!")
+    } else {
+        listanumeros.sort()
+        console.log(listanumeros)
 
-    var menornum = listanumeros[0]
-    console.log(`O menor valor informado foi ${menornum}`)
-    divres.innerHTML += `<p>O menor valor informado foi ${menornum}</p>`
+        var totalnumeros = listanumeros.length
+        console.log(`Ao todo temos ${totalnumeros} números cadastrados.`)
+        divres.innerHTML = `<p>Ao todo temos ${totalnumeros} números cadastrados.</p>`
 
-    /*
-    var maiornumindex = totalnumeros - 1
-    var maiornum = listanumeros[maiornumindex]
-    */
-   var maiornum = 0
-    for (var index in listanumeros) {
-        var numerolista = listanumeros[index]
-        if (numerolista > maiornum) {
-            maiornum = numerolista
+        var menornum = listanumeros[0]
+        console.log(`O menor valor informado foi ${menornum}`)
+        divres.innerHTML += `<p>O menor valor informado foi ${menornum}</p>`
+
+        /*
+        var maiornumindex = totalnumeros - 1
+        var maiornum = listanumeros[maiornumindex]
+        */
+        var maiornum = 0
+        for (var index in listanumeros) {
+            var numerolista = listanumeros[index]
+            if (numerolista > maiornum) {
+                maiornum = numerolista
+            }
         }
+        console.log(`O maior número informado foi ${maiornum}`)
+        divres.innerHTML += `<p>O maior número informado foi ${maiornum}</p>`
+
+        var soma = 0
+        for (var index in listanumeros) {
+            soma += listanumeros[index]
+        }
+        console.log(`Somando todos os valores, temos ${soma}`)
+        divres.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+
+        var media = soma / totalnumeros
+        console.log(`A média dos valores digitados é ${media}`)
+        divres.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
     }
-    console.log(`O maior número informado foi ${maiornum}`)
-    divres.innerHTML += `<p>O maior número informado foi ${maiornum}</p>`
-
-    var soma = 0
-    for (var index in listanumeros) {
-        soma += listanumeros[index]
-    }
-    console.log(`Somando todos os valores, temos ${soma}`)
-    divres.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
-
-    var media = soma / totalnumeros
-    console.log(`A média dos valores digitados é ${media}`)
-    divres.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
-    
-
 }
