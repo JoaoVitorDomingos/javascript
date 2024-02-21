980cab326941c8aac848ec22c4993d7cfdb907d2
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2024-02-21 18:54:09 -0300

    AJAX

---
+++ AJAX/ex001/main.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    document.getElementById("btn").addEventListener("click", btn_click)
+}
+
+function btn_click() {
+    //document.getElementById("div").innerHTML = "Este é o texto alterado."
+
+
+    // AJAX
+
+    let pedido = new XMLHttpRequest() // Criamos o objeto request (como se fosse pegar uma folha e caneta para começar a escrever uma carta)
+
+    pedido.onreadystatechange = function() {
+        document.getElementById("div").innerHTML = this.responseText
+    } // Definir a função que vai atuar mediante a resposta (como se fosse escrever a carta)
+
+    pedido.open("GET", "ddados.txt", true) // Escolhemos o método que vai ser utilizado a requisição, o que queremos e se vai ser de modo assíncrono ou não.
+    pedido.send() // Enviamos a requisição (enviamos a carta)
+}
