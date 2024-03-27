8787d65efbfeaa93a996d556788352e79384d151
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2024-03-27 14:42:58 -0300

    Update ex02.js

--- aula29_break-continue/ex02.js
+++ aula29_break-continue/ex02.js
@@ -1,11 +1,11 @@
-// O Continue irá parar a iteração atual, pulará esta iteração e voltará ao loop. O Continue não para o loop, só a iteração que está atualmente no loop.
+// O Continue irá parar a iteração atual, pulará esta iteração e voltará ao loop. O Continue não para o loop, só a iteração que está atualmente no loop, e passa pra próxima.
 
 let max = 10
 let pares = 0
 
 for(let n = 0; n < max; n++) {
     if(n % 2 != 0) { // Irá checar se o número é par, se for irá adicionar na variável par, se não, está iteração irá ser pulada.
-        console.log(`O número ${n} é impar, então está iteração atual será pulada, voltará ao loop.`)
+        console.log(`O número ${n} é impar, então está iteração atual será parada e irá pra próxima.`)
         continue
     }
 
